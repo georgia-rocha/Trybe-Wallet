2a612f73161cb117134f1e08b22441c1a4b6481e
Georgia <georgiab543@gmail.com>
2023-05-27 18:14:04 -0300

    feat: adicionando git da aplicação e img do teste coverage

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -7,7 +7,7 @@ import { renderWithRouterAndRedux } from './helpers/renderWith';
 describe('Testando a page de Login', () => {
   it('Testa se a página renderiza os inputs de email, senha e botão na tela', () => {
     renderWithRouterAndRedux(<App />);
-    const inputEmail = screen.getByLabelText('Email:');
+    const inputEmail = screen.getByTestId('email-input');
     expect(inputEmail).toBeInTheDocument();
 
     const inputPassword = screen.getByTestId('password-input');
@@ -21,7 +21,7 @@ describe('Testando a page de Login', () => {
     const button = screen.getByRole('button', { name: 'Entrar' });
     expect(button).toBeDisabled();
 
-    const inputEmail = screen.getByLabelText('Email:');
+    const inputEmail = screen.getByTestId('email-input');
     const inputPassword = screen.getByTestId('password-input');
     userEvent.type(inputEmail, 'exemplo@exemplo.com');
     userEvent.type(inputPassword, '123456');
