32e85d4b9e9f0efcc9bc4f912f48b8381d60823e
Georgia <georgiab543@gmail.com>
2023-05-27 18:26:40 -0300

    fix: adiciona basename ao browser router

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './redux/store';
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter>
+    <BrowserRouter basename={ process.env.PUBLIC_URL }>
       <Provider store={ store }>
         <App />
       </Provider>
